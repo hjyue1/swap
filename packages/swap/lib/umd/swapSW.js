1e5e72d7a74496b9b186e7e033cc8eb4491d9109
hjyue1 <hjyue1@163.com>
2022-09-13 09:25:18 +0800

    update: publish

--- packages/swap/lib/umd/swapSW.js
+++ packages/swap/lib/umd/swapSW.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (1.0.0).
+ * Mock Service Worker (1.0.2).
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
  */
